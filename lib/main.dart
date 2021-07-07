41c0342b63ad2a4730ed9e4c4e457c6a18719138
RobertoMdz <roberto.cr.mendoza@gmail.com>
2021-07-07 15:35:04 -0500

    project folders implemented

--- lib/main.dart
+++ lib/main.dart
@@ -8,16 +8,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Material App',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Admin Dashboard'),
-        ),
-        body: Center(
-          child: Container(
-            child: Text('Hello World'),
-          ),
-        ),
-      ),
+      home: Container(),
     );
   }
 }
