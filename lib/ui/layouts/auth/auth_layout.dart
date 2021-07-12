68be7fbfea22eaff04a229470a33fd619a6b676b
RobertoMdz <roberto.cr.mendoza@gmail.com>
2021-07-12 18:54:16 -0500

    adding twitter background in auth layout

--- lib/ui/layouts/auth/auth_layout.dart
+++ lib/ui/layouts/auth/auth_layout.dart
@@ -1,3 +1,4 @@
+import 'package:admin_dashboard/ui/layouts/auth/widgets/background_twitter.dart';
 import 'package:flutter/material.dart';
 
 class AuthLayout extends StatelessWidget {
@@ -6,10 +7,34 @@ class AuthLayout extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        child: Center(
-          child: Text('Auth Layout'),
-        ),
+        body: ListView(
+      children: [
+        _DesktopBody(),
+      ],
+    ));
+  }
+}
+
+class _DesktopBody extends StatelessWidget {
+  const _DesktopBody({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+
+    return Container(
+      width: size.width,
+      height: size.height,
+      color: Colors.red,
+      child: Row(
+        children: [
+          BarckgroundTwitter(),
+          Container(
+            width: 500,
+            height: double.infinity,
+            color: Colors.black,
+          ),
+        ],
       ),
     );
   }
---
+++ lib/ui/layouts/auth/widgets/background_twitter.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class BarckgroundTwitter extends StatelessWidget {
+  const BarckgroundTwitter({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/twitter-bg.png'),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Container(
+          constraints: BoxConstraints(maxWidth: 400),
+          child: Center(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20.0),
+              child: Image(
+                image: AssetImage('assets/twitter-white-logo.png'),
+                width: 400,
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
