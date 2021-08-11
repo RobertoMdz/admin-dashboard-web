d026dc4f12aa0a38b3267105252bc68873078953
RobertoMdz <roberto.cr.mendoza@gmail.com>
2021-08-10 19:27:33 -0500

    implement inputs to add new category item

--- lib/ui/modals/category_modal.dart
+++ lib/ui/modals/category_modal.dart
@@ -1,4 +1,6 @@
 import 'package:admin_dashboard/models/category.dart';
+import 'package:admin_dashboard/ui/buttons/custom_outlined_button.dart';
+import 'package:admin_dashboard/ui/labels/custom_labels.dart';
 import 'package:flutter/material.dart';
 
 class CategoryModal extends StatefulWidget {
@@ -27,9 +29,30 @@ class _CategoryModalState extends State<CategoryModal> {
   Widget build(BuildContext context) {
     return Container(
       height: 500,
-      width: 300,
-      color: Colors.red,
-      child: null,
+      color: Colors.white,
+      padding: EdgeInsets.symmetric(horizontal: 20.0),
+      child: Column(
+        children: [
+          Text(
+            widget.category?.nombre ?? 'Nueva categoria',
+            style: CustomLabels.h1,
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          TextFormField(
+            decoration: InputDecoration(),
+            initialValue: widget.category?.nombre ?? '',
+            onChanged: (value) {
+              nombre = value;
+            },
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          CustomOutlinedButton(onPressed: () {}, text: 'Guardar')
+        ],
+      ),
     );
   }
 }
